bd3c3aa680f0063ef87fd52605f329ed632b0c1d
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-07-17 14:39:44 +0300

    Remove the logic that plays five rounds

--- script.js
+++ script.js
@@ -35,38 +35,5 @@ function checkChoice(choice) {
     return choices.includes(choice);
 }
 
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please enter 'rock', 'paper', or 'scissors'");
-
-        if (!checkChoice(playerSelection)){
-            while(!checkChoice(playerSelection)){
-                playerSelection = prompt("Wrong input, please enter 'rock', 'paper', or 'scissors'");
-            }
-        }
-
-        let computerSelection = getComputerChoice();
-        let roundResult = playASingleRound(playerSelection, computerSelection);
-
-        console.log(roundResult);
-
-        if (roundResult.includes("Win")) {
-            playerScore++;
-        } else if (roundResult.includes("Lose")) {
-            computerScore++;
-        }
-    }
-
-    if (playerScore > computerScore) {
-        console.log(`Congratulations! You won the game. You won ${playerScore} rounds out of 5.`);
-    } else if (computerScore > playerScore) {
-        console.log(`Game over! The computer won this time with ${computerScore} victories out of 5 rounds. Try again?`);
-    } else {
-        console.log(`It's a tie! You and the computer both won ${playerScore} rounds each. Why not play again to break the tie?`);
-    }
-}
 
 playGame();
